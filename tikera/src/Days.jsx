b88eb9740a31ae67d33b6a0691472cf384466a86
TAron003 <aron.toth003@gmail.com>
2025-04-29 21:08:48 +0200

    days worked on

--- tikera/src/Days.jsx
+++ tikera/src/Days.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
 
-const Days = () => {
-  return (
-    <>
-
-    </>
-  )
+function Days () {
+    const d = new Date()
+    const dayID = d.getDay() == 0 ? "day6" : `day${d.getDay() - 1}`
+    const days = [
+        {
+            id: "day0",
+            day: "Hétfő"
+        },
+        {
+            id: "day1",
+            day: "Kedd"
+        },
+        {
+            id: "day2",
+            day: "Szerda"
+        },
+        {
+            id: "day3",
+            day: "Csütörtök"
+        },
+        {
+            id: "day4",
+            day: "Péntek"
+        },
+        {
+            id: "day5",
+            day: "Szombat"
+        },
+        {
+            id: "day6",
+            day: "Vasárnap"
+        }
+    ]
+    console.log(dayID)
+    return (
+        <>
+            <nav>
+                {days.map((day, i) => 
+                <button key={i} id={day.id} className={day.id == dayID ? "bg-emerald-900 hover:bg-emerald-950 p-2.5 m-2.5 rounded-xl active" : "bg-emerald-700 hover:bg-emerald-900 p-2.5 m-2.5 rounded-xl"}>
+                    {day.day}
+                </button>)}
+            </nav>
+        </>
+    )
 }
 
-export default NavBar
+export default Days
